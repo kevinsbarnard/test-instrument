8f19d095949c7e87bb326361607ada3861e5696d
Kevin Barnard <kbarnard@mbari.org>
2023-10-25 17:00:17 +1100

    Add test implementation

---
+++ test.instrument.py
@@ -0,0 +1,27 @@
+from marimba.core.instrument import BaseInstrument
+
+
+class TestInstrument(BaseInstrument):
+    """
+    Test instrument. No-op.
+    """
+    
+    @staticmethod
+    def get_instrument_config_schema() -> dict:
+        return {
+            "my_str_key": "my_str_value",
+            "my_int_key": 1,
+            "my_float_key": 1.0,
+            "my_bool_key": True,
+        }
+    
+    @staticmethod
+    def get_deployment_config_schema() -> dict:
+        return {
+            "year": 2020,
+            "month": 1,
+            "day": 1,
+        }
+
+    def run_rename(self, data_dir, config, **kwargs):
+        print(data_dir, config, kwargs)
